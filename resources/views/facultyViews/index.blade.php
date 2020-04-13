9f1c3d32a280351b4318fc9ba62923b8364f3943
Susan Nebinger <nebingse@miamioh.edu>
2020-04-13 14:38:25 -0400

    Each possible view is now handled in a simple way until the database connection is established

--- resources/views/facultyViews/index.blade.php
+++ resources/views/facultyViews/index.blade.php
@@ -19,22 +19,25 @@
                 <option value="scholarship" >View By Scholarship</option>
                 <option value="pastRecipients" >View Past Scholarship Recipients</option>
                 <option value="specificApplication" >View a Specific Application</option>
-            </select>
+            </select> <br><br><br>
             <div id='scholarshipView' hidden>
                <p><b>Select which Scholarship Applications you would like to view</b></p>
-                <select required name="scholarship" id="scholarship">
+                <select required name="scholarship" id="scholarship" onchange='scholarshipSelected()'>
                     <option value="" disabled selected hidden>Choose A Scholarship: </option>
                     <option value="EY" >EY</option>
                     <option value="KPMG" >KPMG</option>
                     <option value="Worldplay" >Worldplay</option>
                </select>
+               <p id='scholarshipApplication'></p>
             </div>
             <div id='pastRecipientsView' hidden>
                 <p><b>Here are the students who have previously recieved ISA scholarships:</b></p>
+                <p id='pastWinners'></p>
             </div>
             <div id='specificApplicationView' hidden>
                 <p><b>Please enter the name of the student to view their detailed application: </b></p>
-                <input type='text' name='studentName' id='studentName'>
+                <input type='text' name='studentName' id='studentName' onchange='nameEntered()'>
+                <p id='matchingPeople'></p>
             </div>
         </form>
     </div>
@@ -54,6 +57,10 @@
                 document.getElementById('scholarshipView').style.display='none';
                 document.getElementById('pastRecipientsView').style.display='block';
                 document.getElementById('specificApplicationView').style.display='none';
+
+                // display all of the students who have won a scholarship in the past
+                // group by the specific scholarship and sort by year?
+                // put in the paragraph with id of 'pastWinners'
             }
             else if (selectedView == 'specificApplication') {
                 document.getElementById('scholarshipView').style.display='none';
@@ -61,6 +68,22 @@
                 document.getElementById('specificApplicationView').style.display='block';
             }
         };
+
+        function scholarshipSelected() {
+            console.log("GOT HERE");
+            var selectedScholarship = document.getElementById('scholarship').value;
+            console.log(selectedScholarship);
+            document.getElementById('scholarshipApplication').innerHTML = "Viewing applications for the " + selectedScholarship + " scholarship:\n";
+            // select all of the students that applied for the specific scholarship
+        };
+
+        function nameEntered() {
+
+            // if the input is empty, display all submitted scholarships for the current year
+            // otherwise look for students whose name is similar to the entered value
+            var userInput = document.getElementById('studentName').value;
+            document.getElementById('matchingPeople').innerHTML = userInput;
+        }
         
     </script>
 @endsection
